f4be0e70829648e95e626fe4c9e20a483efe5bad
Judy Johnson <judith.johnson@onyxpoint.com>
2015-12-02 16:28:03 -0500

    (SIMP-563) New Process Graph tool
    Reads in process info from file or generates it, and creates
    a .dot and then a .png file diagramming communications
    
    SIMP-563 #comment Updating with some changes
    SIMP-921 #close
    SIMP-779 #close
    SIMP-923 #close
    SIMP-770 #close
    
    Change-Id: I32898b284c4da02a5957e272bcf9963125245a41

---
+++ spec/processgraph_inputdir_spec.rb
@@ -0,0 +1,42 @@
+# Add a testclass
+# run the process
+# make sure the files are created
+#
+require 'simp-processgraph'
+require_relative 'spec_helper'
+require 'fileutils'
+
+site_name = "test_domain"
+home_dir = File.expand_path('~')
+$test_dir = home_dir+'/ssfiles'
+$sample_file = 'spec/fixtures/sample.ss'
+if File.directory?($test_dir) then
+  if !File.exist?($test_dir+'/*.ss')
+     FileUtils.cp($sample_file, $test_dir)
+  end
+else
+  puts "created directory, copied in #{$sample_file}"
+  FileUtils.mkdir($test_dir)
+  FileUtils.cp($sample_file, $test_dir)
+end
+
+describe "testing input directory to process graph" do
+# create a temp dir for output
+  outdir = File.join('processgraph_output',site_name)
+  FileUtils.mkdir_p(outdir) unless File.directory?(outdir)
+
+  Dir.chdir(outdir) do
+    the_graph = ProcessList.new($test_dir, "testdir")
+    the_graph.process_data($test_dir, "testdir", site_name)
+  end
+
+  it "created dot file from input directory name [testdir]" do
+    expect(File).to exist("#{outdir}/testdir.dot")
+  end
+
+  it "created png file from input directory name[testdir]" do
+   expect(File).to exist("#{outdir}/testdir.png")
+  end
+
+end
+
---
+++ spec/simp-processgraph_spec.rb
@@ -0,0 +1,32 @@
+# Add a testclass
+# run the process
+# make sure the files are created
+#
+require 'simp-processgraph'
+require_relative 'spec_helper'
+
+describe "testing process graph with two inputs" do
+# create a temp dir for output
+  site_name = "test_domain"
+  outdir = File.join('processgraph_output',site_name)
+  FileUtils.mkdir_p(outdir) unless File.directory?(outdir)
+
+  Dir.chdir(outdir) do
+    the_graph = ProcessList.new("filein", "fileout")
+    the_graph.process_data("filein", "fileout", site_name)
+  end
+
+  it "created input file [filein] given in and out names" do
+     expect(File).to exist("#{outdir}/filein")
+  end
+
+  it "created dot file [fileout] given in and out names" do
+     expect(File).to exist("#{outdir}/fileout.dot")
+  end
+
+  it "created png file given in and out names" do
+     expect(File).to exist("#{outdir}/fileout.png")
+  end
+
+end
+
